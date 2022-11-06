0e5c183d2bc30b32624c67fd9d905f97924b9a36
Lydia Wildes <lydiawildes1@gmail.com>
2022-11-06 13:10:05 -0600

    edit Card sx values to add margin, add gradient background

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,7 @@ import {HashRouter as Router, Route, } from 'react-router-dom';
 import './App.css';
 import MovieList from '../MovieList/MovieList'
 import MovieDetails from '../MovieDetails/MovieDetails';
+import ActionAreaCard from '../MovieList/MovieList';
 
 function App() {
   return (
--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -2,6 +2,13 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import {HashRouter as Router, Route, Link } from 'react-router-dom';
 import './MovieList.css'
+//importing MUI things
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Grid from "@material-ui/core/Grid";
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
 
 
 function MovieList() {
@@ -17,21 +24,57 @@ function MovieList() {
     return (
         <main>
             <h1>My Film Collection</h1>
+            <Grid container>
+
             <section className="movies">
                 {movies.map(movie => {
                     return ( // each poster/title links to a details page with that movie's ID in the url
-                        <div key={movie.id} > 
+                 
+                            <div  key={movie.id}>
                             <Link to={{pathname: `/details/${movie.id}`}}>
-                            <h3>{movie.title}</h3>
-                            <img src={movie.poster} alt={movie.title}/>
+                                <Card sx={{ width: 230, m: 2, }}>
+                                    <CardActionArea  >
+                                        <CardMedia
+                                            component="img"
+                                            height="330"
+                                            width="250"
+                                            image={movie.poster}
+                                            alt={movie.title}
+                                        />
+                                        <CardContent>
+                                            <Typography 
+                                                gutterBottom variant="p" 
+                                                component="div">
+                                            {movie.title}
+                                            </Typography>
+                                        </CardContent>
+                                    </CardActionArea>
+                                </Card>
                             </Link>
-                        </div>
+                            </div>
+            
+                 
+                      
                     );
                 })}
             </section>
+            </Grid>
         </main>
 
     );
 }
 
+{/* <section className="movies">
+{movies.map(movie => {
+    return ( */}
+{/* <div key={movie.id} > 
+<Link to={{pathname: `/details/${movie.id}`}}>
+<h3>{movie.title}</h3>
+<img src={movie.poster} alt={movie.title}/>
+</Link>
+</div> */}
+// })}
+// </section>
+
 export default MovieList;
+
