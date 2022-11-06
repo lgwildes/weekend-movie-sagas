500902e1c834a1bce9eb9b35f2764d33d9d0be1f
Lydia Wildes <lydiawildes1@gmail.com>
2022-11-06 11:16:21 -0600

    add genre.id key and rearrange movie details

--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -5,7 +5,8 @@ import { useDispatch, useSelector } from 'react-redux';
 function MovieDetails() {
     const dispatch = useDispatch();
     const history = useHistory();
-    const params = useParams();
+    const params = useParams(); //this is getting the params from my /details url
+                                //get the movie with an id that matches the id in my params 
     const movie = useSelector(store => store.movies.find((movie) => movie.id == params.id));
     const genres = useSelector(store => store.movieGenres)
     
@@ -16,15 +17,9 @@ function MovieDetails() {
             type: 'FETCH_GENRES_FOR_MOVIE',
             payload: movie.id
         })
-    },[params.id])
+    },[params.id]) 
 
-//    if (genres !== undefined) {
-//     console.log('genres is', genres)
-    
 
-//    }
-
-  
 
     return (
         <>
@@ -32,14 +27,17 @@ function MovieDetails() {
          onClick={() => {history.push('/')}} >Back to List
         </button>
         <h1>Details here!</h1>
-            <h4>{movie.title}</h4>
-            <h4>Poster</h4>
-            <h4>{movie.description}</h4>
-            {genres.map( genre => (
+            <h4>{movie.title}</h4> 
+            <img src={movie.poster}/> 
+            {genres.map( genre => (  //map and display each genre for movie
+                <div key={genre.id}>
                 <h5>{genre}</h5>
+                </div>
             )
                 
             )}
+            <h4>{movie.description}</h4>
+            
 
         </>
     ) 
