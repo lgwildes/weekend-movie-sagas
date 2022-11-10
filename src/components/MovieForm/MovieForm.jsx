46dc9672b68348df282ca70614c0910104bf0996
Lydia Wildes <lydiawildes1@gmail.com>
2022-11-10 16:13:37 -0600

    add MUI to form

--- src/components/MovieForm/MovieForm.jsx
+++ src/components/MovieForm/MovieForm.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import { useHistory, useParams } from "react-router-dom";
 import { useDispatch, useSelector } from 'react-redux';
 import { useState } from 'react';
-import { Button, Card, CardContent, Grid, FormControl, TextField, TextareaAutosize, Select, MenuItem, InputLabel, Box } from "@material-ui/core";
+import { Button, Card, CardContent, Container, Grid, FormControl, TextField, TextareaAutosize, Select, MenuItem, InputLabel, Box } from "@material-ui/core";
 import './MovieForm.css'
 function MovieForm() {
 
@@ -13,11 +13,11 @@ function MovieForm() {
 
     const dispatch = useDispatch();
     const history = useHistory();
-   
-  
+
+
     useEffect(() => {
         dispatch({ type: 'FETCH_MOVIES' });
-        dispatch({type: 'FETCH_GENRES'}); //these will be intercepted by rootSaga
+        dispatch({ type: 'FETCH_GENRES' }); //these will be intercepted by rootSaga
     }, []);
 
     const genres = useSelector(store => store.genres)
@@ -32,76 +32,84 @@ function MovieForm() {
             description: description,
             genre_id: genre
         }
-            
+
 
         dispatch({
-            type:'CREATE_MOVIE',
+            type: 'CREATE_MOVIE',
             payload: newMovie
         })
 
 
     }
-   
-    
-    if(genres){
+
+
+    if (genres) {
         return (
             <>
-            <div>
-                <Button // button returns user to homepage List of movies
-                    className="button my-super-special-btn"
-                    variant="contained"
-                    onClick={() => {history.push('/')}} >Back to Collection
-                </Button>
+                <div>
+                    <Button // button returns user to homepage List of movies
+                        className="button my-super-special-btn"
+                        variant="contained"
+                        onClick={() => { history.push('/') }} >Back to Collection
+                    </Button>
 
-               
-                      <Card >
-                        <CardContent>
-                        
-                            <form onSubmit={handleSubmit}
+                    <Container maxWidth="sm"  >
+                        <Box>
+                        <Card  >
+                            <CardContent >
+
+                                <form onSubmit={handleSubmit}
                                     className="form">
-                                    <FormControl sx={{ bgcolor: 'white'}}>
-                                    <TextField 
-                                            onChange={(event) => setTitle(event.target.value)} 
+                                    <FormControl >
+                                        <TextField
+                                            onChange={(event) => setTitle(event.target.value)}
                                             variant="outlined"
-                                            type="text" 
-                                            placeholder="film title"/>
-                              
-                                    <TextField onChange={(event) => setPoster(event.target.value)} 
-                                            variant="outlined"
-                                            type="text" 
-                                            placeholder="poster url"/>
+                                            type="text"
+                                            placeholder="film title" />
 
-                                    <TextField
+                                        <TextField onChange={(event) => setPoster(event.target.value)}
+                                            variant="outlined"
+                                            type="text"
+                                            placeholder="poster url" />
+
+                                        <TextField
                                             id="outlined-multiline-flexible"
                                             label="movie description"
                                             multiline
                                             onChange={(event) => setDescription(event.target.value)}
-                                    />
-                              
-                                    <InputLabel htmlFor="genres">select genres</InputLabel>
-                                        <Select onChange={(event) => setGenre(event.target.value)} id="genres" name="genres" value={genre}>
-                                            { genres && genres.map( genre => (
-                                                <MenuItem key={genre.id} value={genre.id}>{genre.name}</MenuItem>
-                                            ) )}
-                                        </Select>
-                               
+                                        />
 
-                                    <Button
-                                        variant="contained" 
-                                        type='submit'>
-                                        save
-                                    </Button>
-                                    <Button 
-                                        variant="contained" 
-                                        onClick={() => {history.push('/')}}>
-                                        cancel
-                                    </Button>
+                                        {/* <InputLabel id="genres" htmlFor="genres">select genres</InputLabel> */}
+                                        <Select onChange={(event) => setGenre(event.target.value)}
+                                            labelId="genres"
+                                            id="genres"
+                                            name="genres"
+                                            value={genre}>
+                                                
+                                            {genres && genres.map(genre => (
+                                                <MenuItem key={genre.id} value={genre.id}>{genre.name}</MenuItem>
+                                            ))}
+                                        </Select>
+
+
+                                        <Button
+                                            variant="contained"
+                                            type='submit'>
+                                            save
+                                        </Button>
+                                        <Button
+                                            variant="contained"
+                                            onClick={() => { history.push('/') }}>
+                                            cancel
+                                        </Button>
                                     </FormControl>
                                 </form>
-                    
+
                             </CardContent>
                         </Card>
-            </div>
+                        </Box>
+                    </Container>
+                </div>
             </>
         )
     }
